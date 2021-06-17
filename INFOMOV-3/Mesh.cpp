a2624df34e66a14cbfb60080cff94d1316c721f6
Joey <joelnl74@gmail.com>
2021-06-17 12:38:05 +0200

    High level optimizations

--- INFOMOV-3/Mesh.cpp
+++ INFOMOV-3/Mesh.cpp
@@ -18,36 +18,39 @@ void Mesh::Draw(Rasterizer &raterizer, EruptionMath::Color color, float time)
 	EruptionMath::vec3 light_direction(0.0f, 0.0f, -1.0f);
 
 	std::vector<EruptionMath::Triangle> toDraw;
+
 	for (auto tri : verticies)
 	{
-		
-		EruptionMath::Triangle vertexShader(EruptionMath::vec3(0, 0, 0), EruptionMath::vec3(0, 0, 0), EruptionMath::vec3(0, 0, 0));
+		EruptionMath::Triangle vertex(EruptionMath::vec3(0, 0, 0), EruptionMath::vec3(0, 0, 0), EruptionMath::vec3(0, 0, 0));
 		
 		if (shader != 0)
 		{
 			shader->SetTime(fTheta);
 			shader->SetPosition(position);
 		}
-		vertexShader.p[0] = shader->VertexShader(tri.p[0]);
-		vertexShader.p[1] = shader->VertexShader(tri.p[1]);
-		vertexShader.p[2] = shader->VertexShader(tri.p[2]);
+		vertex.p[0] = shader->VertexShader(tri.p[0]);
+		vertex.p[1] = shader->VertexShader(tri.p[1]);
+		vertex.p[2] = shader->VertexShader(tri.p[2]);
 
-		vertexShader.color = shader->FragmentShader(color);
+		vertex.color = shader->FragmentShader(color);
 
-		toDraw.push_back(vertexShader);
-
-		//Painters algorithm what should be drawn last, so you see those pixels
-		std::sort(toDraw.begin(), toDraw.end(), [](EruptionMath::Triangle & tri1, EruptionMath::Triangle &tri2)
-		{
-			float z1 = (tri1.p[0].z + tri1.p[1].z + tri1.p[2].z) / 3.0f;
-			float z2 = (tri2.p[0].z + tri2.p[1].z + tri2.p[2].z) / 3.0f;
-			return z1 > z2;
-		});
-
-		for (auto x : toDraw)
-		{
-			raterizer.DrawTriangle(x, x.color.toRGB());
-		}
+		toDraw.push_back(vertex);
 	}
+
+	// Changed move this code from inside the loop to outside the loop, more cache friendly
+	// Painters algorithm what should be drawn last, so you see those pixels
+	std::sort(toDraw.begin(), toDraw.end(), [](EruptionMath::Triangle& tri1, EruptionMath::Triangle& tri2)
+	{
+		float z1 = (tri1.p[0].z + tri1.p[1].z + tri1.p[2].z) / 3.0f;
+		float z2 = (tri2.p[0].z + tri2.p[1].z + tri2.p[2].z) / 3.0f;
+		
+		return z1 > z2;
+	});
+
+	for (auto ver : toDraw)
+	{
+		raterizer.DrawTriangle(ver, ver.color.toRGB());
+	}
+	// Changed move this code from inside the loop to outside the loop, more cache friendly
 }
 
