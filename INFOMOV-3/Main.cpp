0ef4daa8ad4bbd8bbe67cd1f2d91a1733bad4367
Joey <joelnl74@gmail.com>
2021-06-18 22:40:48 +0200

    Test base

--- INFOMOV-3/Main.cpp
+++ INFOMOV-3/Main.cpp
@@ -40,14 +40,16 @@ int main(int argc, char* argv[])
 	unsigned int old_time, current_time = 0;
 	float ftime;
 
+	clock_t current_ticks, delta_ticks;
+	clock_t fps = 0;
+
 	//create window,draw surface and Rasterizer
 	Window *window = new Window();
 	SDL_Surface *screen = SDL_GetWindowSurface(window->window);
 	Rasterizer *rasterizer = new Rasterizer(screen);
 
 	rasterizer->mode = RasterizerMode::Line_And_Fill;
-	Mesh cube(EruptionMath::vec3(400.0f, 300.0f, 0.0f), "Resources/OBJ/teapot.obj" );
-	// Mesh cube1(EruptionMath::vec3(200.0f, 150.0f, 0.0f),"Resources/OBJ/teapot.obj"  );
+	Mesh mesh(EruptionMath::vec3(400.0f, 450.0f, 0.0f), "Resources/OBJ/bunny.obj" );
 
 	EruptionMath::Color color(255 ,255, 255);
 	//red pixel
@@ -59,17 +61,34 @@ int main(int argc, char* argv[])
 		current_time = SDL_GetTicks();
 
 		ftime = (current_time - old_time) / 1000.0f;
-		// printf("%f", ftime);
+
+		// FPS
+		current_ticks = clock();
+		// END FPS
+
 		SDL_Event event;
 		while (SDL_PollEvent(&event))
+		{
 			HandleEvent(event);
 
 			SDL_FillRect(screen, 0, 0);
 			//render here
-		     cube.Draw(*rasterizer, color,ftime);
-			// cube1.Draw(*rasterizer, color, ftime);
+			mesh.Draw(*rasterizer, color, ftime);
+			// bunny2.Draw(*rasterizer, color2, color2, ftime);
+			// bunny3.Draw(*rasterizer, color3, color3, ftime);
+
+			// bunny4.Draw(*rasterizer, color3, color3, ftime);
+			// bunny5.Draw(*rasterizer, color2, color2, ftime);
+			// bunny6.Draw(*rasterizer, color1, color1, ftime);
 
 			SDL_UpdateWindowSurface(window->window);
+		}
+
+
+		delta_ticks = clock() - current_ticks; //the time, in ms, that took to render the scene
+		if (delta_ticks > 0)
+			fps = CLOCKS_PER_SEC / delta_ticks;
+		std::cout << "FPS: " << fps << std::endl;
 	}
 	// Close and destroy the window
 	SDL_DestroyWindow(window->window);
